1f3c555009d1aecd724ea373b30ad2970b784379
Nikita Belevich <belevish_1998@mail.ru>
2020-10-11 21:43:28 +0300

    First commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+// Функция возвращает промис, который либо получил данные, либо завершился ошибкой
+function sendRequest(URL, method = 'GET', data, responseType = 'text') {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, URL);
+
+        // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        xhr.responseType = responseType;
+        if (method == 'POST') {
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xhr.send(data);
+        } else {
+            xhr.send();
+        }
+        
+
+        // Получен ответ
+        xhr.onload = () => {
+            // Если код не 200, то завершаем промис ошибкой HTTP
+            if (xhr.status != 200) {
+                reject(new Error(`HTTP status: ${xhr.status}\n URL: ${URL}`));
+            }
+            resolve(xhr.response);
+        }
+        // обработаем ошибку, не связанную с HTTP (например, нет соединения)
+        xhr.onerror = () => {
+            
+        }
+        // выведем прогресс
+        xhr.onprogress = (event) => {
+            
+        }
+    });
+}
+
+
+
+
